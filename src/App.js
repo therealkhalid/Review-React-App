abd806080fc330f980ee69489216443f970ca8d0
tourhzaoui khalid <111646098+therealkhalid@users.noreply.github.com>
2023-05-17 16:28:21 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,37 @@
-import React from 'react';
-import AvergaReating from './Components/AvergaReating';
-import Form from './Components/Form';
-import Header from './Components/Header';
-import ReviewList from './Components/ReviewList';
+import React from "react";
+import AvergaReating from "./Components/AvergaReating";
+import Form from "./Components/Form";
+import Header from "./Components/Header";
+import ReviewList from "./Components/ReviewList";
 function App() {
-  const[reviews,setReviews]=React.useState([])
-  const addReview=(review)=>{
-    setReviews([review,...reviews])
-  }
-  const RemoveReview=(id)=>{
-    setReviews(reviews.filter(function(review){
-      return review.id !==id
-    }))
-  }
+  const [reviews, setReviews] = React.useState([]);
+  const addReview = (review) => {
+    setReviews([review, ...reviews]);
+  };
+  const RemoveReview = (id) => {
+    setReviews(
+      reviews.filter(function (review) {
+        return review.id !=== id;
+      })
+    );
+  };
   return (
-    <div className='row my-5'>
-    <div className='col-md-8 m-auto'>
-      <div className='card'>
-        <div className='card-header bg-secondary'><Header/></div>
-        <div className='div card-body text-warning bg-dark'>
-          <Form addReview={addReview}/>
-          <AvergaReating reviews={reviews}/>
-          <ReviewList reviews={reviews} RemoveReview={RemoveReview}/>
+    <div className="container">
+      <div className="row my-5">
+        <div className="col-md-8 m-auto">
+          <div className="card">
+            <div className="card-header bg-secondary">
+              <Header />
+            </div>
+            <div className="div card-body text-warning bg-dark">
+              <Form addReview={addReview} />
+              <AvergaReating reviews={reviews} />
+              <ReviewList reviews={reviews} RemoveReview={RemoveReview} />
+            </div>
+          </div>
         </div>
-
       </div>
     </div>
-    
-    
-    
-  
-  </div>
   );
 }
 
